9c25aae10f8c656ba6d4cd3d8b1a193a9df1ee58
ww <2011358693@qq.com>
2021-07-10 17:30:11 +0800

    小程序全局配置和页面配置及其app.js生命周期函数

---
+++ app.js
@@ -0,0 +1,40 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log('小程序初始化完成');
+    // 获取用户信息   异步操作
+    // wx.getUserInfo({
+    //   success: function (res) {
+    //     console.log(res);
+    //   }
+    // })
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log('小程序显示');
+
+    setTimeout(function() {
+      throw Error('错误')
+    }, 1000)
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log('小程序隐藏');
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    console.log('小程序发生错误');
+  }
+})
